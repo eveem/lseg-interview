e73eb75435f733d63d4a739f1e512761e807c0da
Monpriya <monpriya.t@ibmdt.co.th>
2025-01-22 23:12:20 +0700

    add template, app and static files

---
+++ app/app.py
@@ -0,0 +1,21 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+dogs = [
+    { "name": "Lunar", "image": "lunar.jpg" },
+    { "name": "Solar", "image": "solar.jpg" },
+    { "name": "Stellar", "image": "stellar.jpg" },
+]
+
+current_dog_index = 0
+
+@app.route("/")
+def home():
+    global current_dog_index
+    dog = dogs[current_dog_index]
+    current_dog_index = (current_dog_index + 1) % len(dogs)
+    return render_template("index.html", dog_name=dog["name"], dog_image=dog["image"])
+
+if __name__ == "__main__":
+    app.run(debug=True)
