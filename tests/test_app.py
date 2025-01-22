1794e1ed2cd40ebbb84a7cf12e300d8e198be566
Monpriya <monpriya.t@ibmdt.co.th>
2025-01-22 23:12:35 +0700

    add test hompage

---
+++ tests/test_app.py
@@ -0,0 +1,27 @@
+import unittest
+from app.app import app
+
+class AppTests(unittest.TestCase):
+    def setUp(self):
+        self.app = app.test_client()
+        self.app.testing = True
+    
+    def test_homepage(self):
+        response = self.app.get("/")
+        self.assertEqual(response.status_code, 200)
+    
+    def test_cycle_dogs(self):
+        response = self.app.get("/")
+        self.assertIn(b"lunar.jpg", response.data)
+
+        response = self.app.get("/")
+        self.assertIn(b"solar.jpg", response.data)
+
+        response = self.app.get("/")
+        self.assertIn(b"stellar.jpg", response.data)
+
+        response = self.app.get("/")
+        self.assertIn(b"lunar.jpg", response.data)
+
+if __name__ == "__main__":
+    unittest.main()
