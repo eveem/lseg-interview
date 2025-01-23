dd557de0f08fa01111204d1d7ea11605536a5a9e
Monpriya <monpriya.t@ibmdt.co.th>
2025-01-23 18:06:01 +0700

    revert

--- Dockerfile
+++ Dockerfile
@@ -1,7 +1,9 @@
-FROM public.ecr.aws/lambda/python:3.13
+FROM python:3.13-slim
 
 WORKDIR /app 
 COPY . /app
 RUN pip install --no-cache-dir -r requirements.txt
 
-CMD ["app.lambda_handler"]
+EXPOSE 5000
+
+CMD ["python", "app/app.py"]
--- app/app.py
+++ app/app.py
@@ -1,38 +1,21 @@
-# from flask import Flask, render_template
-# from aws_lambda_powertools.event_handler import api_gateway
-# from aws_lambda_powertools.logging import Logger
-
-# app = Flask(__name__)
-
-# dogs = [
-#     { "name": "Lunar", "image": "lunar.jpg" },
-#     { "name": "Solar", "image": "solar.jpg" },
-#     { "name": "Stellar", "image": "stellar.jpg" },
-# ]
-
-# current_dog_index = 0
-# logger = Logger()
-
-# @app.route("/")
-# def home():
-#     global current_dog_index
-#     dog = dogs[current_dog_index]
-#     current_dog_index = (current_dog_index + 1) % len(dogs)
-#     return render_template("index.html", dog_name=dog["name"], dog_image=dog["image"])
-
-# def lambda_handler(event, context):
-#     handler = api_gateway.ApiGatewayHandler()
-#     logger.info(f"Received event: {event}")
-#     return handler.handle(event, context)
-
-from flask import Flask, jsonify
-from aws_lambda_wsgi import response
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
-@app.route('/')
-def hello_world():
-    return jsonify(message="Hello, World!")
+dogs = [
+    { "name": "Lunar", "image": "lunar.jpg" },
+    { "name": "Solar", "image": "solar.jpg" },
+    { "name": "Stellar", "image": "stellar.jpg" },
+]
 
-def lambda_handler(event, context):
-    return response(app, event, context)
+current_dog_index = 0
+
+@app.route("/")
+def home():
+    global current_dog_index
+    dog = dogs[current_dog_index]
+    current_dog_index = (current_dog_index + 1) % len(dogs)
+    return render_template("index.html", dog_name=dog["name"], dog_image=dog["image"])
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000)
