daa8f4857480031073eab5c05a692453c2c784e7
Monpriya <monpriya.t@ibmdt.co.th>
2025-01-23 16:30:33 +0700

    update port

--- Dockerfile
+++ Dockerfile
@@ -4,6 +4,6 @@ WORKDIR /app
 COPY . /app
 RUN pip install --no-cache-dir -r requirements.txt
 
-EXPOSE 80
+EXPOSE 5000
 
 CMD ["python", "app/app.py"]
--- app/app.py
+++ app/app.py
@@ -18,4 +18,4 @@ def home():
     return render_template("index.html", dog_name=dog["name"], dog_image=dog["image"])
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=80)
+    app.run(host="0.0.0.0", port=5000)
